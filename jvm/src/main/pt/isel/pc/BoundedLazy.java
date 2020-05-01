7eb71c3f108a14a8c13eaf501900b00358b6590d
João Matos <joao.matos.jm@gmail.com>
2020-05-01 20:51:02 +0100

    Done Serie 1 Ex1

---
+++ jvm/src/main/pt/isel/pc/BoundedLazy.java
@@ -0,0 +1,105 @@
+package pt.isel.pc;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+
+public class BoundedLazy<E> {
+    private Supplier<E> supplier;
+    private int lives;
+    private int maxLives;
+
+    private E res = null;
+    private boolean isCalculating = false;
+    private boolean isException = false;
+    private Exception exception = null;
+
+    private final Lock mon = new ReentrantLock();
+    private final Condition cond = mon.newCondition();
+
+    public BoundedLazy(Supplier<E> supplier, int lives){
+        this.supplier = supplier;
+        this.lives = lives;
+        this.maxLives = lives;
+    }
+
+    public Optional<E> get(long timeout) throws Exception {
+        try {
+            mon.lock();
+
+            //Check if there is an exception
+            if(isException)
+                throw exception;
+
+            //Happy path
+            if(res != null && lives > 0){
+                lives--;
+                return Optional.of(res);
+            }
+
+            //See if current value is being calculated
+            if(isCalculating){
+                long limit = Timeouts.start(timeout);
+                long remaining = Timeouts.remaining(limit);
+                while (true) {
+                    //Start wait
+                    cond.await(remaining, TimeUnit.MILLISECONDS);
+
+                    //Check if there was an exception
+                    if(isException)
+                        throw exception;
+
+                    //Check if value was calculated and lives remain
+                    if(res != null && lives > 0){
+                        lives--;
+                        return Optional.of(res);
+                    }
+
+                    //See if it being calculated again. If not, leave loop and have this thread do it
+                    if(!isCalculating){
+                        isCalculating = true;
+                        break;
+                    }
+
+                    //Leave wait loop if timeout reached
+                    remaining = Timeouts.remaining(limit);
+                    if (Timeouts.isTimeout(remaining)) {
+                        return Optional.empty();
+                    }
+                }
+            }
+            //If not, this thread will do it
+            else {
+                isCalculating = true;
+            }
+        } finally {
+            mon.unlock();
+        }
+
+        //Start calculating value
+        try {
+            res = supplier.get();
+
+            lives = maxLives - 1;
+            isCalculating = false;
+            return Optional.of(res);
+        }
+        catch (Exception e){
+            isCalculating = false;
+            isException = true;
+            exception = e;
+            throw e;
+        }
+        finally {
+            try {
+                mon.lock();
+                cond.signalAll();
+            } finally {
+                mon.unlock();
+            }
+        }
+    }
+}
---
+++ jvm/src/test/pt/isel/pc/BoundedLazyTest.java
@@ -0,0 +1,54 @@
+package pt.isel.pc;
+
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class BoundedLazyTest {
+
+    @Test
+    public void simpleBoundedLazyTest() throws InterruptedException {
+
+        Supplier<Integer> supplier = () -> {
+            try {
+                Thread.sleep((long) (Math.random() * 10 * 1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return (int) (Math.random() * 100);
+        };
+
+        BoundedLazy boundedLazy = new BoundedLazy(supplier, 4);
+        List<Thread> ths = new LinkedList<>();
+
+        for (int i = 0; i < 22; i++) {
+            Thread th = new Thread(() -> {
+                try {
+                    Optional<Integer> res = boundedLazy.get((long) (Math.random() * 50 * 1000));
+                    System.out.println(res);
+                } catch (Exception e) {
+                    assert false;
+                }
+            });
+            th.start();
+            ths.add(th);
+        }
+        for (Thread th : ths) {
+            th.join();
+        }
+        assert true;
+    }
+
+//    Supplier<Integer> supplier = () -> {
+//        try {
+//            Thread.sleep((long) (Math.random() * 10 * 1000));
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return (int) (Math.random() * 100);
+//    };
+}
