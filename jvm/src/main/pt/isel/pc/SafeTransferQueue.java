08657e2d9f0a272c2b8375dac63e60dc0c0427cc
João Matos <joao.matos.jm@gmail.com>
2020-05-28 17:35:17 +0100

    Done Serie2 Ex2

---
+++ jvm/src/main/pt/isel/pc/SafeTransferQueue.java
@@ -0,0 +1,57 @@
+package pt.isel.pc;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SafeTransferQueue<E> {
+    Node<E> sentinel = new Node<E>(null);
+    private final AtomicReference<Node<E>> head = new AtomicReference<>(sentinel);
+    private final AtomicReference<Node<E>> tail = new AtomicReference<>(sentinel);
+
+    public SafeTransferQueue() {
+    }
+
+    public void put(E message){
+        Node<E> newTail = new Node<>(message);
+        while (true) {
+            Node<E> observedTail = tail.get();
+            Node<E> observedTailNext = observedTail.next.get();
+            if (observedTail == tail.get()) {	// confirm that we have a good tail, to prevent CAS failures
+                if (observedTailNext != null) { /** step A **/
+                    // queue in intermediate state, so advance tail for some other thread
+                    boolean res = tail.compareAndSet(observedTail, observedTailNext);		/** step B **/
+                } else {
+                    // queue in quiescent state, try inserting new node
+                    if (observedTail.next.compareAndSet(null, newTail)) {	/** step C **/
+                        // advance the tail
+                        boolean res2 = tail.compareAndSet(observedTail, newTail);	/** step D **/
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public E take(){
+        while(true){
+            Node<E> observedHead = head.get();
+            Node<E> newHead = observedHead.next.get();
+            if(newHead != null){
+                if(head.compareAndSet(observedHead, newHead)) {
+                    return newHead.data;
+                }
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private static class Node<E> {
+        final AtomicReference<Node<E>> next;
+        final E data;
+
+        Node(E data) {
+            next = new AtomicReference<>(null);
+            this.data = data;
+        }
+    }
+}
---
+++ jvm/src/test/pt/isel/pc/SafeTransferQueueTest.java
@@ -0,0 +1,95 @@
+package pt.isel.pc;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SafeTransferQueueTest {
+    static int num = 1;
+
+    @Test
+    public void simpleSafeTransferQueueTest() throws InterruptedException {
+        SafeTransferQueue<String> transferQueue = new SafeTransferQueue<>();
+        List<Thread> ths = new LinkedList<>();
+
+        TakeMessage(transferQueue, null, ths);
+        PutMessage(transferQueue, ths);
+        PutMessage(transferQueue, ths);
+        TakeMessage(transferQueue, "PutMessage 1", ths);
+        PutMessage(transferQueue, ths);
+        TakeMessage(transferQueue, "PutMessage 2", ths);
+        TakeMessage(transferQueue, "PutMessage 3", ths);
+        TakeMessage(transferQueue,null, ths);
+        TakeMessage(transferQueue,null, ths);
+        PutMessage(transferQueue, ths);
+        TakeMessage(transferQueue, "PutMessage 4", ths);
+        PutMessage(transferQueue, ths);
+        TakeMessage(transferQueue, "PutMessage 5", ths);
+
+        for (Thread th : ths) {
+            th.join();
+        }
+    }
+
+    @Test
+    public void randomSafeTransferQueueTest() throws InterruptedException {
+        SafeTransferQueue<String> transferQueue = new SafeTransferQueue<>();
+        List<Thread> ths = new LinkedList<>();
+        final int nOfThreads = 20;
+
+        for (int i = 1; i < nOfThreads; i++) {
+            int finalI = i;
+            Thread th = new Thread(() -> transferQueue.put("PutMessage #" + finalI));
+            ths.add(th);
+        }
+
+        for (int i = 1; i < nOfThreads - 3; i++) {
+            int finalI = i;
+            Thread th = new Thread(() -> {
+                try {
+                    Thread.sleep((long) (Math.random() * 2 * 1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String res = transferQueue.take();
+                if(res != null)
+                    System.out.println("Take #" + finalI + " got message '" + res + "'");
+                else
+                    System.out.println("Take #" + finalI + " no message to take");
+            });
+            ths.add(th);
+        }
+
+        Collections.shuffle(ths);
+        for (Thread th : ths) {
+            th.start();
+        }
+        for (Thread th : ths) {
+            th.join();
+        }
+        assert true;
+    }
+
+    private void TakeMessage(SafeTransferQueue<String> transferQueue, String expectedValue, List<Thread> ths) throws InterruptedException {
+        Thread th = new Thread(() -> {
+            String res = transferQueue.take();
+            System.out.println(res);
+            if(res == null)
+                assert expectedValue == null;
+            else
+                assert res.equals(expectedValue);
+        });
+        th.start();
+        ths.add(th);
+        Thread.sleep(1000);
+    }
+
+    private void PutMessage(SafeTransferQueue<String> transferQueue, List<Thread> ths) throws InterruptedException {
+        Thread th = new Thread(() -> transferQueue.put("PutMessage " + num++));
+        th.start();
+        ths.add(th);
+        Thread.sleep(1000);
+    }
+}
